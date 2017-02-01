75500bcc264859ec18d0b1ed03f31c39eeb33152
JJ Santos <jj@Juans-MacBook-Pro.local>
2017-02-01 07:58:04 -0600

    new proyects added

---
+++ Numeros Naturales Perfectos/Numeros Naturales Perfectos/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Numeros Naturales Perfectos
+//
+//  Created by JJ Santos on 2/1/17.
+//  Copyright © 2017 JJ Santos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
